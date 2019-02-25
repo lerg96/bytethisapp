9bc39bccb729810fb917dcd5fd5f6c7f0ed56885
Luis Romero <lerg96@gmail.com>
2019-02-25 02:51:45 -0400

    agregador una ruta para la api de la nueva app

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,17 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('data', function () {
+    $data = collect([
+    'bitcoin_usd'=> 4000,
+    'ethereum_usd'=> 140,
+    'bitcoin_ves_1h'=> 3000,
+    'bitcoin_ves_12h'=> 3500,
+    'bitcoin_ves_24h'=> 3300,
+    'miner_hashrate_actual'=> 48.3,
+    'miner_hashrate_reportado'=> 40.8,
+    'wallet'=> 0.0015,
+    'miner_balance'=> 0.31
+]);
+return response()->json($data);
+});
